195f501a340fada88f799133743b80de8408efad
Edwin <edwinenjo.o@gmail.com>
2022-06-30 19:36:33 -0700

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,15 @@
-#include <studio.h>
+#include <stdio.h>
+
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
+
 return (0);
 }
