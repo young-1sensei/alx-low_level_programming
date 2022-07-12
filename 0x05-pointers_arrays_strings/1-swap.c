fe035d704300d41ae8fe158a3db5188e0212604d
Edwin <edwinenjo.o@gmail.com>
2022-07-12 09:44:42 -0700

    Task 1 Write a function that swaps the values of two integers.

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * swap_int - function that swaps the values of two intergers.
+ * swap_int - function that swaps the values of two integers.
  * @a: Pointer input
  * @b: Pointer input
  */
-void swap_int(int *a, int*b)
+void swap_int(int *a, int *b)
 {
 	int tmp;
 
